d9b84b6100c7d69a96c403bdc2ff8768e3d5e7f8
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-14 22:05:36 -0600

    update

--- lua/pitchblack/lualine/init.lua
+++ lua/pitchblack/lualine/init.lua
@@ -2,16 +2,13 @@ local config = require("pitchblack.config")
 
 local M = {}
 
-function M.extend_opts()
+function M.extend_opts(lualine_opts)
 	if not config.options.lualine.enabled then
 		return
 	end
 
-	pcall(function()
-		if config.options.lualine.enabled then
-			require("pitchblack.lualine").extend_opts(require("lualine").get_config())
-		end
-	end)
+	lualine_opts.options = lualine_opts.options or {}
+	lualine_opts.options = vim.tbl_deep_extend("force", {}, lualine_opts.options, config.options.lualine.opts or {})
 end
 
 return M
