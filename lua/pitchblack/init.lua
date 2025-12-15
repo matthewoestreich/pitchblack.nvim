e28c4a5111f9a244c90439ecf85c5f67e2fd61f2
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-15 15:24:28 -0600

    update

--- lua/pitchblack/init.lua
+++ lua/pitchblack/init.lua
@@ -4,24 +4,24 @@ local hl = require("pitchblack.highlights")
 local M = {}
 
 function M.setup(opts)
-	vim.notify("pitchblack:config:setup()")
+	--vim.notify("pitchblack:config:setup()")
 	vim.g.colors_name = "pitchblack"
 	config.setup(opts)
 	M.apply()
 end
 
 function M.apply()
-	vim.notify("pitchblack:config:apply()")
+	--vim.notify("pitchblack:config:apply()")
 	hl.apply_core()
 	hl.apply_treesitter()
 	require("pitchblack.integrations.rainbow").apply()
 end
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "pitchblack",
-	callback = function()
-		vim.schedule(M.setup)
-	end,
-})
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+-- 	pattern = "pitchblack",
+-- 	callback = function()
+-- 		vim.schedule(M.setup)
+-- 	end,
+-- })
 
 return M
