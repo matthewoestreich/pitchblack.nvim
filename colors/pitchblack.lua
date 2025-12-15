9dbb85888067baff4401ac64170c3eaad7a8483c
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-15 13:22:39 -0600

    update

--- colors/pitchblack.lua
+++ colors/pitchblack.lua
@@ -4,7 +4,9 @@ if vim.fn.exists("syntax_on") == 1 then
 	vim.cmd("syntax reset")
 end
 
+vim.cmd("hi clear")
+
 vim.g.colors_name = "pitchblack"
 
 vim.o.termguicolors = true
-require("pitchblack").setup({})
+require("pitchblack").setup()
