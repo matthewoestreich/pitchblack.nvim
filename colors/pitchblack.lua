fdfc88c0349511833d563b7a7dfd9859b153c5af
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-15 13:31:20 -0600

    update

--- colors/pitchblack.lua
+++ colors/pitchblack.lua
@@ -1,7 +1,7 @@
-vim.cmd("highlight clear")
-vim.cmd("hi clear")
+--vim.cmd("highlight clear")
+--vim.cmd("hi clear")
 vim.g.colors_name = "pitchblack"
 vim.o.termguicolors = true
 
 local pb = require("pitchblack")
-pb.setup()
+pb.setup({})
