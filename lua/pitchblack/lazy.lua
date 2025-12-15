374be5767476eeb5b38360138573e9dce4bb1098
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-14 21:55:39 -0600

    update

--- lua/pitchblack/lazy.lua
+++ lua/pitchblack/lazy.lua
@@ -2,8 +2,16 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		optional = true,
-		opts = function(_, opts)
-			require("pitchblack.lualine").extend_opts(opts)
+		config = function()
+			local ok, lualine = pcall(require, "lualine")
+			if not ok then
+				return
+			end
+			-- Merge pitchblack options safely
+			local cfg = lualine.get_config()
+			cfg.options =
+				vim.tbl_deep_extend("force", {}, cfg.options, require("pitchblack.config").options.lualine.opts or {})
+			lualine.setup(cfg)
 		end,
 	},
 }
