cace4c8b12e07370de4f6f207423e2b801a73069
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-14 21:30:10 -0600

    update

--- lua/pitchblack/init.lua
+++ lua/pitchblack/init.lua
@@ -12,9 +12,6 @@ function M.apply()
 	hl.apply_core()
 	hl.apply_treesitter()
 	require("pitchblack.integrations.rainbow").apply()
-	if config.options.lualine.enabled then
-		require("pitchblack.lualine").extend_opts(require("lualine").get_config())
-	end
 end
 
 return M
